c19bbe4b16909165432be9ba28f73029bdfd7e5b
yashnuhash <nuhashroxme@gmail.com>
2023-06-05 01:33:44 +0600

    feat:Practiced till Python-string

--- testing.py
+++ testing.py
@@ -1,2 +1,22 @@
+import random
+
 print("Hello World!");
 print("Started Learing in Python !!!");
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(random.randrange(1,10))
+
+s = "Ashraful-Nuhash"
+print(s[1])
+
+txt = "The best things in life are free!"
+print("!" in txt)
+
+
